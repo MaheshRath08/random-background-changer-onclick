3a6a4d4c184832d315c3f4d4a96c65cdf2a4d025
MaheshRath08 <maheshrath.2002@gmail.com>
2022-06-26 23:38:59 +0530

    added some comments

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let btn = document.getElementById("btn")
 
 function randomColor(){
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);         //generates a random decimal code
 }
 
 btn.addEventListener("click", function(){
-    document.body.style.backgroundColor = randomColor()
+    document.body.style.backgroundColor = randomColor()     //call the function with the DOM 
 })
